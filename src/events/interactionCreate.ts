5f3fc3ea374312554a40a6b65aba5a04d596227a
Vuxxs <vamvakaspanayiotis@gmail.com>
2022-08-09 11:47:21 +0300

    interaction permissions check

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,10 +1,28 @@
+import { ChannelType } from "discord.js";
 import CustomClient from "src/modules/customClient";
 import { interactions } from "src/modules/interactions";
 
 export default (client: CustomClient): void => {
   client.on("interactionCreate", async (interaction) => {
-    if (!interaction.isChatInputCommand()) return;
+    if (
+      !interaction.isChatInputCommand() ||
+      !interaction.channel ||
+      !interaction.guild
+    )
+      return;
 
-    interactions[interaction.commandName](interaction);
+    if (interaction.channel.type === ChannelType.GuildText) {
+      if (
+        interaction.guild.members
+          .me!.permissionsIn(interaction.channel)
+          .has(["SendMessages", "ViewChannel"])
+      ) {
+        interactions[interaction.commandName](interaction);
+      } else {
+        interaction.reply(
+          "Can't reply here! I can't view or write to this channel."
+        );
+      }
+    }
   });
 };
