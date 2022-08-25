ea77866f581330068c1b6242acc11315cc9da930
Vuxxs <vamvakaspanayiotis@gmail.com>
2022-08-25 04:23:51 +0300

    update channel id

--- src/chat/triggers/1010021933271756852/welcome.ts
+++ src/chat/triggers/1010021933271756852/welcome.ts
@@ -3,7 +3,7 @@ import { Message } from "discord.js";
 function welcomeDaredevil(msg: Message) {
   if (msg.guild!.id !== "1010021933271756852") return;
 
-  if (msg.channel.id !== "1012159778069418065") return;
+  if (msg.channel.id !== "1010021933720551496") return;
 
   if (msg.content.toLowerCase() === "elegant sux") {
     msg.channel.send(
