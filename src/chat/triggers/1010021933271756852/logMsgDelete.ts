4fa43fbfb6f178c18c7d78b5ddee186c48debea8
Vuxxs <vamvakaspanayiotis@gmail.com>
2022-08-27 22:48:29 +0300

    critical logs fix

--- src/chat/triggers/1010021933271756852/logMsgDelete.ts
+++ src/chat/triggers/1010021933271756852/logMsgDelete.ts
@@ -5,7 +5,9 @@ function daredevilMsgDeleteLog(msg: Message) {
   if (msg.guild!.id !== "1010021933271756852") return;
   if (msg.member!.roles.cache.get("1012093261306941545")) return;
 
-  const channel = msg.channel;
+  const channel = msg.guild!.channels.cache.get(
+    "1012029657400492314"
+  ) as GuildTextBasedChannel;
   if (channel) {
     const embed = createLogEmbed(msg, "**Deleted Message**");
     channel.send({ embeds: [embed] });
